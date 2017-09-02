26aa9dd6f7e319023368f88446c261a8809d0fb4
ncullen93 <ncullen.th@dartmouth.edu>
2017-09-02 12:35:05 -0400

    train w/ augmentation

--- code/training/train_model_sampler.py
+++ code/training/train_model_sampler.py
@@ -23,9 +23,11 @@ data_dir = base_dir + 'data/'
 input_tx = tx.MinMaxScaler((0,1)) # scale input images between 0 and 1
 target_tx = tx.BinaryMask(cutoff=0.5) # convert target segmentation to a binary mask
 
+# tx.Compose lets you string together multiple transforms
 co_tx = tx.Compose([tx.ExpandDims(axis=-1), # expand from (128,128) to (128,128,1) -> RandomAffine and Keras expect that
-                    tx.RandomAffine(fill_value='min',rotation_range=(-15,15), # rotate btwn -15 & 15 degrees
-                                    translation_range=(0.1,0.1), # translate btwn 0-10% horiz, 0-10% vert
+                    tx.RandomAffine(fill_value='min', # can be integer also
+                                    rotation_range=(-15,15), # rotate btwn -15 & 15 degrees
+                                    translation_range=(0.1,0.1), # translate btwn -10% and 10% horiz, -10% and 10% vert
                                     shear_range=(-10,10), # shear btwn -10 and 10 degrees
                                     zoom_range=(0.85,1.15)) # between 15% zoom-in and 15% zoom-out
                     ])
@@ -56,8 +58,9 @@ model = create_unet_model2D(input_image_size=train_data[0][0].shape, n_labels=1,
 callbacks = [cbks.ModelCheckpoint(base_dir+'weights-py.h5', monitor='val_loss', save_best_only=True),
             cbks.ReduceLROnPlateau(monitor='val_loss', factor=0.1)]
 
-model.fit_generator(generator=train_loader, steps_per_epoch=np.ceil(len(train_data)/batch_size), 
+model.fit_generator(generator=iter(train_loader), steps_per_epoch=np.ceil(len(train_data)/batch_size), 
                     epochs=100, verbose=1, callbacks=callbacks, 
-                    shuffle=True, validation_data=val_loader, validation_steps=np.ceil(len(val_data)/batch_size), 
+                    shuffle=True, 
+                    validation_data=iter(val_loader), validation_steps=np.ceil(len(val_data)/batch_size), 
                     class_weight=None, max_queue_size=10, 
                     workers=1, use_multiprocessing=False,  initial_epoch=0)
